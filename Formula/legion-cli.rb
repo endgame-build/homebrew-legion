1e88227ec7d0349ae05ac02b54949db8e0fb1a9c
Formula Update <action@github.com>
2025-07-01 16:09:10 +0300

    fix: update formula to eliminate dependency issues
    - Remove all manual resource definitions
    - Use pip to handle dependencies automatically
    - Fix 404 errors and ModuleNotFoundError issues

--- Formula/legion-cli.rb
+++ Formula/legion-cli.rb
@@ -12,82 +12,23 @@ class LegionCli < Formula
   version "0.1.dev20250630+gb7158f0"
 
   depends_on "python@3.12"
-  depends_on "uv" => :optional
-
-  resource "typer" do
-    url "https://files.pythonhosted.org/packages/42/9a/03707b4b3324b02de7110d1cb6b2ba6a13141b11a6a1ca3b06b7e6c53fb4e/typer-0.16.0.tar.gz"
-    sha256 "595e5a2f2a46fb0d1d1b0198e853c319673b8664132fe7c0fd7eb58d09ca15d2"
-  end
-
-  resource "jinja2" do
-    url "https://files.pythonhosted.org/packages/af/92/b3130cbbf5591acf9ade8708c365f3238046ac7cb8ccba6e81abccb0ccff/Jinja2-3.1.5.tar.gz"
-    sha256 "595e5a2f2a46fb0d1d1b0198e853c319673b8664132fe7c0fd7eb58d09ca15d2"
-  end
-
-  resource "python-dotenv" do
-    url "https://files.pythonhosted.org/packages/bc/57/e84d88dea8131d5b7a75e9c8bafaf46fc37e81cde7d6aa3b8b8e4a8cd0e3/python-dotenv-1.1.0.tar.gz"
-    sha256 "595e5a2f2a46fb0d1d1b0198e853c319673b8664132fe7c0fd7eb58d09ca15d2"
-  end
-
-  resource "pyyaml" do
-    url "https://files.pythonhosted.org/packages/54/ed/79a089b6be93607fa5cdaedf301d7dfb23af5f25c398d5ead2525b063e17/pyyaml-6.0.2.tar.gz"
-    sha256 "595e5a2f2a46fb0d1d1b0198e853c319673b8664132fe7c0fd7eb58d09ca15d2"
-  end
-
-  resource "jsonschema" do
-    url "https://files.pythonhosted.org/packages/38/2e/03362ee4034a4c917f697890ccd4aec0800ccf9ded7f511971c75451deec/jsonschema-4.23.0.tar.gz"
-    sha256 "595e5a2f2a46fb0d1d1b0198e853c319673b8664132fe7c0fd7eb58d09ca15d2"
-  end
-
-  resource "click" do
-    url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.8.tar.gz"
-    sha256 "595e5a2f2a46fb0d1d1b0198e853c319673b8664132fe7c0fd7eb58d09ca15d2"
-  end
-
-  resource "gitpython" do
-    url "https://files.pythonhosted.org/packages/e5/c2/6e3a26945a7ff7cf2854b8825026cf3dd07b681c63e5e8a4a75e0d9139d2/GitPython-3.1.44.tar.gz"
-    sha256 "595e5a2f2a46fb0d1d1b0198e853c319673b8664132fe7c0fd7eb58d09ca15d2"
-  end
-
-  resource "markupsafe" do
-    url "https://files.pythonhosted.org/packages/b4/d2/38ff920762f2247c3af5cbbbbc40756f575d9692d381d7c520f45deb9b8f/MarkupSafe-3.0.2.tar.gz"
-    sha256 "595e5a2f2a46fb0d1d1b0198e853c319673b8664132fe7c0fd7eb58d09ca15d2"
-  end
-
-  resource "jira" do
-    url "https://files.pythonhosted.org/packages/78/b4/557e4c80c0ea12164ffeec0e29372c085bfb263faad53cef5e1455523bec/jira-3.8.0.tar.gz"
-    sha256 "595e5a2f2a46fb0d1d1b0198e853c319673b8664132fe7c0fd7eb58d09ca15d2"
-  end
-
-  resource "jira2markdown" do
-    url "https://files.pythonhosted.org/packages/69/fd/22635ac878174973a016e1acdd9252e3dafd6a54e64487a8d7479ec4acfc/jira2markdown-0.5.tar.gz"
-    sha256 "595e5a2f2a46fb0d1d1b0198e853c319673b8664132fe7c0fd7eb58d09ca15d2"
-  end
-
-  resource "mistune" do
-    url "https://files.pythonhosted.org/packages/ef/c8/f0173fe3bf85fd891aee2e7bcd8207dfe26c2c683d727c5a6cc3aec7b628/mistune-3.0.2.tar.gz"
-    sha256 "595e5a2f2a46fb0d1d1b0198e853c319673b8664132fe7c0fd7eb58d09ca15d2"
-  end
-
-  resource "toml" do
-    url "https://files.pythonhosted.org/packages/be/ba/1f744cdc819428fc6b5084ec34d9b30660f6f9daaf70eead706e3203ec3c/toml-0.10.2.tar.gz"
-    sha256 "595e5a2f2a46fb0d1d1b0198e853c319673b8664132fe7c0fd7eb58d09ca15d2"
-  end
-
-  resource "typing-extensions" do
-    url "https://files.pythonhosted.org/packages/f6/37/23083fcd6e35492953e8d2aaaa68b860eb422b34627b13f2ce3eb6106061/typing_extensions-4.13.2.tar.gz"
-    sha256 "595e5a2f2a46fb0d1d1b0198e853c319673b8664132fe7c0fd7eb58d09ca15d2"
-  end
 
   def install
-    # Create virtualenv
+    # Create a virtualenv in libexec
     venv = virtualenv_create(libexec, "python3.12")
     
-    # Install dependencies first
-    venv.pip_install resources
+    # Install the package and all dependencies
+    system libexec/"bin/pip", "install", "--upgrade", "pip"
+    system libexec/"bin/pip", "install", buildpath
     
-    # Install legion-cli
-    venv.pip_install_and_link buildpath
+    # Create wrapper script that ensures proper Python environment
+    (bin/"legion").write <<~EOS
+      #!/bin/bash
+      exec "#{libexec}/bin/python" -m legion.main "$@"
+    EOS
+    
+    # Make it executable
+    (bin/"legion").chmod 0755
   end
 
   def caveats
